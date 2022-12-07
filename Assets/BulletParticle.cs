76da3dbe1336b959090ff68c76be349920fce32d
jehto2023 <jehto@ucsc.edu>
2022-12-07 13:16:34 -0800

    added gun, bullet particles, left click to shoot script

---
+++ Assets/BulletParticle.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletParticle : MonoBehaviour
+{
+    public ParticleSystem particleSystem;
+
+    List<ParticleCollisionEvent> colEvents = new List<ParticleCollisionEvent>();
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            particleSystem.Play();
+        }
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        int events = particleSystem.GetCollisionEvents(other, colEvents);
+
+        Debug.Log("Hit");
+
+        //for (int i = 0; i < events; i++)
+        //{
+
+        //}
+    }
+}
